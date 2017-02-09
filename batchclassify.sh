f03acba5c1b7f55bc63f10e8d620a426a31da6d3
David Mawdsley <david.mawdsley@manchester.ac.uk>
2017-02-09 14:12:28 +0000

    Test script to verify R results

---
+++ batchclassify.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Run abc-classify in non-interactive mode
+# 
+# $1 participantCode
+# $2 trainingframes
+
+videodir=/media/sf_spot_the_difference/video/
+trackerdir=/media/sf_spot_the_difference/openface/
+extgtdir=/home/zzalsdme/IDInteraction/spot_the_difference/part1/
+
+pcs=_front
+extgts=checkGTwebcam2.csv 
+
+startframe=`head -1 ${extgtdir}${1}${extgts} |awk -F"," '{print $1}'`
+
+endframe=`tail -1 ${extgtdir}${1}${extgts} |awk -F"," '{print $1}'`
+echo $startframe $endframe
+
+
+./abc-classify.py --videofile ${videodir}${1}${pcs}.mp4 --trackerfile ${trackerdir}${1}${pcs}.openface --startframe $startframe --endframe $endframe --extgt ${extgtdir}${1}${extgts} --externaltrainingframes $2 --outfilelocalpreds ${1}_local_${2}.csv --outfileexternalpreds ${1}_external_${2}.csv --useexternalgt --saverngstate ${1}_${2}_rng.bin
+
