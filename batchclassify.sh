30b547926a33778b4a61bcea4928a1ff8613f091
David Mawdsley <david.mawdsley@manchester.ac.uk>
2017-02-10 09:27:53 +0000

    Output fewer files when running batch classifcation
    (Files were overwritten on every iteration anyway)

--- batchclassify.sh
+++ batchclassify.sh
@@ -18,8 +18,9 @@ endframe=`tail -1 ${extgtdir}${1}${extgts} |awk -F"," '{print $1}'`
 echo $startframe $endframe
 
 
-./abc-classify.py --videofile ${videodir}${1}${pcs}.mp4 --trackerfile ${trackerdir}${1}${pcs}.openface --startframe $startframe --endframe $endframe --extgt ${extgtdir}${1}${extgts} --externaltrainingframes $2 --outfilelocalpreds ${1}_local_${2}.csv --outfileexternalpreds ${1}_external_${2}.csv --useexternalgt --saverngstate ${1}_${2}_rng.bin --participantcode ${1}shuffle --summaryfile summaryresults.csv
+./abc-classify.py --videofile ${videodir}${1}${pcs}.mp4 --trackerfile ${trackerdir}${1}${pcs}.openface --startframe $startframe --endframe $endframe --extgt ${extgtdir}${1}${extgts} --externaltrainingframes $2 --useexternalgt --participantcode ${1}shuffle --summaryfile summaryresults.csv
 
 
-./abc-classify.py --videofile ${videodir}${1}${pcs}.mp4 --trackerfile ${trackerdir}${1}${pcs}.openface --startframe $startframe --endframe $endframe --extgt ${extgtdir}${1}${extgts} --externaltrainingframes $2 --outfilelocalpreds ${1}_local_${2}.csv --outfileexternalpreds ${1}_external_${2}.csv --useexternalgt --saverngstate ${1}_${2}_rng.bin --participantcode ${1}noshuffle --summaryfile summaryresults.csv --noshuffle
+./abc-classify.py --videofile ${videodir}${1}${pcs}.mp4 --trackerfile ${trackerdir}${1}${pcs}.openface --startframe $startframe --endframe $endframe --extgt ${extgtdir}${1}${extgts} --externaltrainingframes $2 --useexternalgt --participantcode ${1}noshuffle --summaryfile summaryresults.csv --noshuffle
+
 
